a2a62821e1c2339bb17077296c72055e306a04a8
krishnateja <Krishnatejajinka@gmail.com>
2025-10-07 22:51:51 +0100

    reverse string

---
+++ Arrays (E/M)/reverseString.js
@@ -0,0 +1,18 @@
+// Leetcode problem no. 344
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    const temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+};
