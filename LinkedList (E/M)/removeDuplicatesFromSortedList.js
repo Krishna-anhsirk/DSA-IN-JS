879ec851272951539d67191fffa918d2bf64c12c
krishnateja <Krishnatejajinka@gmail.com>
2025-10-30 13:36:25 +0000

    remove duplicates from sorted linked list

---
+++ LinkedList (E/M)/removeDuplicatesFromSortedList.js
@@ -0,0 +1,27 @@
+// Leetcode problem no. 83
+
+// My solution. Made a mistake for more than 2 duplicates case. Added else to make it work. Made the same mistake 3 years ago. But, could rectify it after seeing the failure case
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.next.val === curr.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return head;
+};
